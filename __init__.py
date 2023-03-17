6bc3184aa6a1b5e247204dcd2087e5bbb3442cf1
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2023-03-17 15:23:02 -0500

    Update __init__.py
    add webhooks

--- __init__.py
+++ __init__.py
@@ -130,6 +130,11 @@ class Client:
         url = f'{self.base_url}/servers/{serverid}'
         response = self.request('GET', url)
         return response
+    
+    def get_server_members(self, serverid):
+        url = f'{self.base_url}/servers/{serverid}/members'
+        response = self.request('GET', url)
+        return response
 
     def create_listitem(self, channelid, title, note=None):
         data = {'message': title}
@@ -171,4 +176,41 @@ class Client:
         url = f'{self.base_url}/channels/{channelid}/items/{listitemid}/complete'
         response = self.request('POST', url)
         return response
+    
+        def create_webhook(self, serverid, channelid, name):
+        url = f'{self.base_url}/servers/{serverid}/webhooks'
+        data = {'name': name, 'channelId': channelid}
+        response = self.request('POST', url, json=data)
+        return response
+
+    def update_webhook(self, serverid, webhookid, name, channelid=None):
+        url = f'https://www.guilded.gg/api/v1/servers/{serverid}/webhooks/{webhookid}'
+        data = {'name': name}
+        if channelid:
+            data.update({'channelId': channelid})
+        response = self.request('PUT', url, json=data)
+        return response
+
+    def delete_webhook(self, serverid, webhookid):
+        url = f'https://www.guilded.gg/api/v1/servers/{serverid}/webhooks/{webhookid}'
+        response = self.request('DELETE', url)
+        return response
+
+    def get_webhook(self, serverid, webhookid):
+        url = f'https://www.guilded.gg/api/v1/servers/{serverid}/webhooks/{webhookid}'
+        response = self.request('GET', url)
+        return response
+
+    def get_webhooks(self, serverid, channelid):
+        url = f'https://www.guilded.gg/api/v1/servers/{serverid}/webhooks'
+        data = {'channelId': channelid}
+        response = self.request('GET', url, json=data)
+        return response
+
+    def send_webhook_message(self, serverid, webhookid, content):
+        webhookinformation = self.get_webhook(serverid, webhookid)
+        token = webhookinformation['webhook']['token']
+        url = f'https://media.guilded.gg/webhooks/{webhookid}/{token}'
+        data = {'content': content}
+        self.request('POST', url, json=data)
 
