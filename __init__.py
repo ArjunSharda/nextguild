80238d7630b546d291b83929c1236584ee776d64
J3pe <108808053+J3pe@users.noreply.github.com>
2023-03-18 17:00:12 +0100

    reactions and docs

--- __init__.py
+++ __init__.py
@@ -367,8 +367,110 @@ class Client:
     url = f'https://www.guilded.gg/api/v1/channels/{channelid}/topics/{forumtopicid}/comments/{forumcommentid}'
     response = self.request('DELETE', url)
     return response
+  
+  def create_reaction(self, channelid, contentid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/content/{contentid}/emotes/{emoteid}'
+    response = self.request('PUT', url)
+    return response
+  
+  def delete_reaction(self, channelid, contentid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/content/{contentid}/emotes/{emoteid}'
+    response = self.request('DELETE', url)
+    return response
+  
+  def create_topic_reaction(self, channelid, topicid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/topics/{topicid}/emotes/{emoteid}'
+    response = self.request('PUT', url)
+    return response
 
+  def delete_topic_reaction(self, channelid, topicid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/topics/{topicid}/emotes/{emoteid}'
+    response = self.request('DELETE', url)
+    return response
+  
+  def create_topic_comment_reaction(self, channelid, topicid, topiccommentid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/topics/{topicid}/comments/{topiccommentid}/emotes/{emoteid}'
+    response = self.request('PUT', url)
+    return response
+  
+  def delete_topic_comment_reaction(self, channelid, topicid, topiccommentid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/topics/{topicid}/comments/{topiccommentid}/emotes/{emoteid}'
+    response = self.request('DELETE', url)
+    return response
 
+  def create_event_reaction(self, channelid, eventid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/events/{eventid}/emotes/{emoteid}'
+    response = self.request('PUT', url)
+    return response
+  
+  def delete_event_reaction(self, channelid, eventid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/events/{eventid}/emotes/{emoteid}'
+    response = self.request('DELETE', url)
+    return response
+  
+  def create_event_comment_reaction(self, channelid, eventid, commentid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/events/{eventid}/comments/{commentid}/emotes/{emoteid}'
+    response = self.request('PUT', url)
+    return response
+  
+  def delete_event_comment_reaction(self, channelid, eventid, commentid, emoteid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/events/{eventid}/comments/{commentid}/emotes/{emoteid}'
+    response = self.request('DELETE', url)
+    return response
+  
+  #here's gonna be more stuff
+  
+  def create_doc(self, channelid, title, content):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/docs'
+    data = {'title': title, 'content': content}
+    response = self.request('POST', url, json=data)
+    return response
+  
+  def get_docs(self, channelid, before=None, limit=None):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/docs'
+    params = {}
+    if before:
+      params['before'] = before
+    if limit:
+      params['limit'] = limit
+    response = self.request('GET', url, params=params)
+    return response
+  
+  def get_doc(self, channelid, docid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/docs/{docid}'
+    response = self.request('GET', url)
+    return response
+
+  def update_doc(self, channelid, docid, title=None, content=None):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/docs/{docid}'
+    data = {}
+    if title:
+      data['title'] = title
+    if content:
+      data['content'] = content
+    response = self.request('PUT', url, json=data)
+    return response
+  
+  def delete_doc(self, channelid, docid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/docs/{docid}'
+    response = self.request('DELETE', url)
+    return response
+  
+  def create_doc_comment(self, channelid, docid, content):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/docs/{docid}/comments'
+    data = {'content': content}
+    response = self.request('POST', url, json=data)
+    return response
+  def update_doc_comment(self, channelid, docid, commentid, content):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/docs/{docid}/comments/{commentid}'
+    data = {'content': content}
+    response = self.request('PATCH', url, json=data)
+    return response
+  def delete_doc_comment(self, channelid, docid, commentid):
+    url = f'https://www.guilded.gg/api/v1/channels/{channelid}/docs/{docid}/comments/{commentid}'
+    response = self.request('DELETE', url)
+    return response
+  
     
 class message:
 
@@ -396,3 +498,5 @@ def get_id(obj):
       result = get_id(item)
       if result is not None:
         return result
+
+
