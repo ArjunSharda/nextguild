1c797925060dfa3ea93ef606a8e933dd7caa08ba
J3pe <108808053+J3pe@users.noreply.github.com>
2023-03-19 02:14:02 +0100

    bunch of updates

--- nextguild/__init__.py
+++ nextguild/__init__.py
@@ -181,12 +181,12 @@ class Client:
         return response
     
     
-    def get_member_ban(self, serverid, userid):
+    def get_server_ban(self, serverid, userid):
         url = f'{self.base_url}/servers/{serverid}/bans/{userid}'
         response = self.request('GET', url)
         return response
     
-    def get_member_bans(self, serverid):
+    def get_server_bans(self, serverid):
         url = f'{self.base_url}/servers/{serverid}/bans'
         response = self.request('GET', url)
         return response
@@ -542,6 +542,34 @@ class Client:
         return response
     def get_roles(self, serverid, userid):
         url = f'https://www.guilded.gg/api/v1/servers/{serverid}/members/{userid}/roles'
+        response = self.request('GET', url)
+        return response
+    
+    def assign_group(self, groupid, userid):
+        url = f'https://www.guilded.gg/api/v1/groups/{groupid}/members/{userid}'
+        response = self.request('PUT', url)
+        return response
+    def remove_group(self, groupid, userid):
+        url = f'https://www.guilded.gg/api/v1/groups/{groupid}/members/{userid}'
+        response = self.request('DELETE', url)
+        return response
+    def award_user_xp(self, serverid, userid, amount):
+        url = f'{self.base_url}/servers/{serverid}/members/{userid}/xp'
+        data = {'total': amount}
+        response = self.request('PUT', url, json=data)
+        return response
+    def set_user_xp(self, serverid, userid, total):
+        url = f'{self.base_url}/servers/{serverid}/members/{userid}/xp'
+        data = {'total': total}
+        response = self.request('PUT', url, json=data)
+        return response
+    def award_role_xp(self, serverid, roleid, amount):
+        url = f'{self.base_url}/servers/{serverid}/roles/{roleid}/xp'
+        data = {'amount': amount}
+        response = self.request('POST', url, json=data)
+        return response
+    def get_user_social(self, serverid, userid, socialtype):
+        url = f'{self.base_url}/servers/{serverid}/members/{userid}/social-links/{socialtype}'
         response = self.request('GET', url)
         return response
 
