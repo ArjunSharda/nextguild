366ef6e139e5226100af4a4c0d6cb763a695c1e3
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2023-03-22 17:19:41 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,23 @@
-from setuptools import setup, find_packages
-import pathlib
+[build-system]
+requires = ["setuptools", "requests", "asyncio", "websockets"]
+build-backend = "setuptools.build_meta"
 
-project_dir = pathlib.Path(__file__).parent.resolve()
+[project]
+name = "nextguild"
+version = "1.0.1"
+authors = [
+    { name="Arjun Sharda", email="sharda.aj17@gmail.com" },
+]
+description = "Interactions with the Guilded API made simpler"
+readme = "README.md"
+license = { file="LICENSE" }
+requires-python = ">=3.7"
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+]
 
-# Get the long description from the README file
-long_description = (project_dir / "README.md").read_text(encoding="utf-8")
-
-setup(
-    name="nextguild",
-    version="1.0.0",
-    description="Simplify interactions with the Guilded API",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/ArjunSharda/nextguild",
-    author="Arjun Sharda",
-    author_email="sharda.aj17@gmail.com",
-    classifiers=[  # Optional
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Natural Language :: English",
-        "Topic :: Communications :: Chat",
-        
-    ],
-
-
-
-    package_dir={"": "nextguild"},
-  
-  
-    packages=find_packages(where="src"),  # Required
-
-    python_requires=">=3.7",
-
-    install_requires=["requests", "asyncio", "websockets"],
-
-    project_urls={
-    "Homepage":"https://github.com/ArjunSharda/nextguild",
-    "Bug Tracker":"https://github.com/ArjunSharda/nextguild/issues"
-    },
-)
+[project.urls]
+"Homepage" = "https://github.com/ArjunSharda/nextguild"
+"Bug Tracker" = "https://github.com/ArjunSharda/nextguild/issues"
