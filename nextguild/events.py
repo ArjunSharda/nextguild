158465c8e697b8c5768992b1c4cd813f57c8fb65
Arjun Sharda <77706434+ArjunSharda@users.noreply.github.com>
2023-03-27 10:54:26 -0500

    Update events.py

--- nextguild/events.py
+++ nextguild/events.py
@@ -6,20 +6,38 @@ from functools import wraps
 import websockets
 
 from nextguild.client import Client
-from nextguild.message import Message
+from nextguild.message import Message, Webhook
+from nextguild.reaction import Reaction, CalendarReaction, ForumTopicComentReaction
+from nextguild.channel import Channel
+
 
 
 class Events:
-    def __init__(self, client: Client):
+    def __init__(self, client):
         self._message_create_handlers = []
         self._message_update_handlers = []
         self._message_delete_handlers = []
         self._member_join_handlers = []
         self._member_leave_handlers = []
+        self._bot_join_handlers = []
+        self._bot_leave_handlers = []
         self._member_banned_handlers = []
         self._member_unbanned_handlers = []
         self._ready_handlers = []
+        self._reaction_create_handlers = []
+        self._reaction_delete_handlers = []
+        self._forum_topic_comment_reaction_create_handlers = []
+        self._forum_topic_comment_reaction_delete_handlers = []
+        self._calendar_event_reaction_create_handlers = []
+        self._calendar_event_reaction_delete_handlers = []
+        self._channel_create_handlers = []
+        self._channel_delete_handlers = []
+        self._channel_update_handlers = []
+        self._webhook_create_handlers = []
+        self._webhook_delete_handlers = []
+        self._webhook_update_handlers = []
         self.client = client
+
 
     def on_message(self, func):
         @wraps(func)
@@ -29,8 +47,8 @@ class Events:
         self._message_create_handlers.append(wrapper)
         return wrapper
 
-    async def _handle_create_message(self, event_data: dict):
-        message = Message(event_data)
+    async def _handle_create_message(self, eventData):
+        message = Message(eventData)
         for handler in self._message_create_handlers:
             await handler(message)
 
@@ -42,8 +60,8 @@ class Events:
         self._message_update_handlers.append(wrapper)
         return wrapper
 
-    async def _handle_update_message(self, event_data: dict):
-        message = Message(event_data)
+    async def _handle_update_message(self, eventData):
+        message = Message(eventData)
         for handler in self._message_update_handlers:
             await handler(message)
 
@@ -55,8 +73,8 @@ class Events:
         self._message_delete_handlers.append(wrapper)
         return wrapper
 
-    async def _handle_delete_message(self, event_data: dict):
-        message = Message(event_data)
+    async def _handle_delete_message(self, eventData):
+        message = Message(eventData)
         for handler in self._message_delete_handlers:
             await handler(message)
 
@@ -68,9 +86,10 @@ class Events:
         self._member_join_handlers.append(wrapper)
         return wrapper
 
-    async def _handle_member_join(self, event_data: dict):
+
+    async def _handle_member_join(self, eventData):
         for handler in self._member_join_handlers:
-            await handler(event_data)
+            await handler(eventData)
 
     def on_member_leave(self, func):
         @wraps(func)
@@ -80,9 +99,9 @@ class Events:
         self._member_leave_handlers.append(wrapper)
         return wrapper
 
-    async def _handle_member_leave(self, event_data: dict):
+    async def _handle_member_leave(self, eventData):
         for handler in self._member_leave_handlers:
-            await handler(event_data)
+            await handler(eventData)
 
     def on_member_banned(self, func):
         @wraps(func)
@@ -92,9 +111,9 @@ class Events:
         self._member_banned_handlers.append(wrapper)
         return wrapper
 
-    async def _handle_member_banned(self, event_data: dict):
+    async def _handle_member_banned(self, eventData):
         for handler in self._member_banned_handlers:
-            await handler(event_data)
+            await handler(eventData)
 
     def on_member_unbanned(self, func):
         @wraps(func)
@@ -104,9 +123,9 @@ class Events:
         self._member_unbanned_handlers.append(wrapper)
         return wrapper
 
-    async def _handle_member_unbanned(self, event_data: dict):
+    async def _handle_member_unbanned(self, eventData):
         for handler in self._member_unbanned_handlers:
-            await handler(event_data)
+            await handler(eventData)
 
     def on_ready(self, func):
         @wraps(func)
@@ -120,31 +139,194 @@ class Events:
         for handler in self._ready_handlers:
             await handler()
 
+    def on_reaction_create(self, func):
+        @wraps(func)
+        def wrapper(reaction):
+            return func(reaction)
+
+        self._reaction_create_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_create_reaction(self, eventData):
+        reaction = Reaction(eventData)
+        for handler in self._reaction_create_handlers:
+            await handler(reaction)
+
+    def on_reaction_delete(self, func):
+        @wraps(func)
+        def wrapper(reaction):
+            return func(reaction)
+
+        self._reaction_delete_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_delete_reaction(self, eventData):
+        reaction = Reaction(eventData)
+        for handler in self._reaction_delete_handlers:
+            await handler(reaction)
+
+    def on_forum_topic_comment_reaction_create(self, func):
+        @wraps(func)
+        def wrapper(reaction):
+            return func(reaction)
+
+        self._forum_topic_comment_reaction_create_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_forum_topic_comment_reaction_create(self, eventData):
+        reaction = ForumTopicCommentReaction(eventData)
+        for handler in self._forum_topic_comment_reaction_create_handlers:
+            await handler(reaction)
+
+    def on_forum_topic_comment_reaction_delete(self, func):
+        @wraps(func)
+        def wrapper(reaction):
+            return func(reaction)
+
+        self._forum_topic_comment_reaction_delete_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_forum_topic_comment_reaction_delete(self, eventData):
+        reaction = ForumTopicCommentReaction(eventData)
+        for handler in self._forum_topic_comment_reaction_delete_handlers:
+            await handler(reaction)
+
+    def on_calendar_event_reaction_create(self, func):
+        @wraps(func)
+        def wrapper(reaction):
+            return func(reaction)
+
+        self._calendar_event_reaction_create_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_calendar_event_reaction_create(self, eventData):
+        reaction = CalendarReaction(eventData)
+        for handler in self._calendar_event_reaction_create_handlers:
+            await handler(reaction)
+
+    def on_calendar_event_reaction_delete(self, func):
+        @wraps(func)
+        def wrapper(reaction):
+            return func(reaction)
+
+        self._calendar_event_reaction_delete_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_calendar_event_reaction_delete(self, eventData):
+        reaction = CalendarReaction(eventData)
+        for handler in self._calendar_event_reaction_delete_handlers:
+            await handler(reaction)
+
+    def on_channel_create(self, func):
+        @wraps(func)
+        def wrapper(channel):
+            return func(channel)
+
+        self._channel_create_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_create_channel(self, eventData):
+      channel = Channel(eventData)
+      for handler in self._channel_create_handlers:
+        await handler(channel)
+
+
+    def on_channel_delete(self, func):
+        @wraps(func)
+        def wrapper(channel):
+            return func(channel)
+
+        self._channel_delete_handlers.append(wrapper)
+        return wrapper
+
+
+    async def _handle_delete_channel(self, eventData):
+        channel = Channel(eventData)
+        for handler in self._channel_delete_handlers:
+            await handler(channel)
+
+    def on_channel_update(self, func):
+        @wraps(func)
+        def wrapper(channel):
+            return func(channel)
+
+        self._channel_update_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_update_channel(self, eventData):
+        channel = Channel(eventData)
+        for handler in self._channel_update_handlers:
+            await handler(channel)
+
+
+    def on_webhook_create(self, func):
+        @wraps(func)
+        def wrapper(webhook):
+            return func(webhook)
+
+        self._webhook_create_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_create_webhook(self, eventData):
+        webhook = Webhook(eventData)
+        for handler in self._webhook_create_handlers:
+            await handler(webhook)
+
+    def on_webhook_update(self, func):
+        @wraps(func)
+        def wrapper(webhook):
+            return func(webhook)
+
+        self._webhook_update_handlers.append(wrapper)
+        return wrapper
+
+    async def _handle_update_webhook(self, eventData):
+        webhook = Webhook(eventData)
+        for handler in self._webhook_update_handlers:
+            await handler(webhook)
+
+
+
+
+
+
     async def start(self):
-        async with websockets.connect(
-                'wss://www.guilded.gg/websocket/v1',
-                extra_headers={'Authorization': f'Bearer {self.client.token}'}
-        ) as websocket:
+        async with websockets.connect('wss://www.guilded.gg/websocket/v1',
+                                      extra_headers={'Authorization': f'Bearer {self.client.token}'}) as websocket:
             await self._handle_ready()
             while True:
                 data = await websocket.recv()
                 json_data = json.loads(data)
 
                 if 't' in json_data and 'd' in json_data:
-                    event_type: str = json_data['t']
-                    event_data: dict = json_data['d']
-                    event_handlers = {
-                        'ChatMessageCreated': self._handle_create_message,
-                        'ChatMessageUpdated': self._handle_update_message,
-                        'ChatMessageDeleted': self._handle_delete_message,
-                        'ServerMemberJoined': self._handle_member_join,
-                        'ServerMemberRemoved': self._handle_member_leave,
-                        'ServerMemberBanned': self._handle_member_banned,
-                        'ServerMemberUnbanned': self._handle_member_unbanned,
-                    }
-                    handler = event_handlers.get(event_type)
-                    if handler:
-                        await handler(event_data)
+                    eventType, eventData = json_data['t'], json_data['d']
+                else:
+                    continue
+
+                event_handlers = {
+                    'ChatMessageCreated': self._handle_create_message,
+                    'ChatMessageUpdated': self._handle_update_message,
+                    'ChatMessageDeleted': self._handle_delete_message,
+                    'ServerMemberJoined': self._handle_member_join,
+                    'ServerMemberRemoved': self._handle_member_leave,
+                    'ServerMemberBanned': self._handle_member_banned,
+                    'ServerMemberUnbanned': self._handle_member_unbanned,
+                    'ChannelMessageReactionCreated': self._handle_create_reaction,
+                    'ChannelMessageReactionDeleted': self._handle_delete_reaction,
+                    'ForumTopicCommentReactionCreated': self._handle_forum_topic_comment_reaction_create,
+                    'ForumTopicCommentReactionDeleted': self._handle_forum_topic_comment_reaction_delete,
+                    'CalendarEventReactionCreated': self._handle_calendar_event_reaction_create,
+                    'CalendarEventReactionDeleted': self._handle_calendar_event_reaction_delete,
+                    'ServerChannelCreated': self._handle_create_channel,
+                    'ServerChannelDeleted': self._handle_delete_channel,
+                    'ServerChannelUpdated': self._handle_update_channel,
+                    'ServerWebhookCreated': self._handle_create_webhook,
+                    'ServerWebhookUpdated': self._handle_update_webhook
+
+                }
+                handler = event_handlers.get(eventType)
+                if handler:
+                    await handler(eventData)
 
     def run(self):
         try:
