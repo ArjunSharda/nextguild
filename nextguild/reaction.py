a95ac46f2bc57171c41070217e84e4bedb8c6c43
Jakub Suchenek <jakub.suchenek.25@gmail.com>
2023-03-28 19:13:22 +0200

    Inherit Reaction

--- nextguild/reaction.py
+++ nextguild/reaction.py
@@ -10,28 +10,17 @@ class Reaction:
         self.emote_url: str = event_data.get('reaction', {}).get('emote', {}).get('url')
 
 
-class CalendarReaction:
+class CalendarReaction(Reaction):
     def __init__(self, event_data: dict):
-        self.event_data: dict = event_data
-        self.server_id: str = event_data.get('serverId')
-        self.channel_id: str = event_data.get('reaction', {}).get('channelId')
-        self.user_id: str = event_data.get('reaction', {}).get('createdBy')
-        self.emote_name: str = event_data.get('reaction', {}).get('emote', {}).get('name')
-        self.emote_id: str = event_data.get('reaction', {}).get('emote', {}).get('id')
         self.emote_url: str = event_data.get('reaction', {}).get('emote', {}).get('url')
         self.calendar_event_id: str = event_data.get('reaction', {}).get('calendarEventId')
+        super().__init__(event_data)
 
           
-class ForumTopicCommentReaction:
-    def __init__(self, event_data: dict):
-        self.event_data: dict = event_data
-        self.server_id: str = event_data.get('serverId')
-        self.channel_id: str = event_data.get('reaction', {}).get('channelId')
-        self.user_id: str = event_data.get('reaction', {}).get('createdBy')
-        self.emote_name: str = event_data.get('reaction', {}).get('emote', {}).get('name')
-        self.emote_id: str = event_data.get('reaction', {}).get('emote', {}).get('id')
-        self.emote_url: str = event_data.get('reaction', {}).get('emote', {}).get('url')
         self.forum_topic_id: str = event_data.get('reaction', {}).get('forumTopicId')
         self.forum_topic_comment_id: str = event_data.get('reaction', {}).get('forumTopicCommentId')
 
  
+class ForumTopicCommentReaction(Reaction):
+    def __init__(self, event_data: dict):
+        super().__init__(event_data)
