437f4e169f617562129fb0f70f06c690f1e1aeb1
J3pe <108808053+erik-thorsell@users.noreply.github.com>
2023-04-29 14:59:13 +0200

    Update message.py

--- nextguild/message.py
+++ nextguild/message.py
@@ -27,7 +27,6 @@ class Message:
         self.is_silent: bool = message.get('isSilent', False)
         # If message has no mentions it's empty
         # TODO Model "mentions" attribute
-        print(message)
         self.mentions = [mention['id'] for mention in message.get('mentions', {}).get('users', [])]
         self.created_at: str = message.get('createdAt')
         self.author_id: str = message.get('createdBy')
